c57fa3e3aa9736da7a842091ad073118d6652057
Jeroen Mies <jeroen.mies@gmail.com>
2017-10-11 13:12:53 +0200

    Styling improved

--- frontend/src/components/Root.js
+++ frontend/src/components/Root.js
@@ -1,20 +1,24 @@
 import React, {Component} from 'react';
 import {Provider} from "react-redux";
 import {ConnectedBookList} from "../containers/BookList";
+import logo from '../logo.svg';
+import '../styles/app.scss';
 
 export class Root extends Component {
 
   render() {
-
-    return <div>
-      <h1>Books Application</h1>
-
+    return (
       <Provider store={this.props.store}>
-        <ConnectedBookList/>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1>Books Application</h1>
+          </div>
+
+          <ConnectedBookList/>
+        </div>
       </Provider>
-
-    </div>
-
+    )
   }
 
 }
